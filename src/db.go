952585afff4d41e21f40a017bbd876a7a2b7e8fa
Martin Danhier <martin.danhier@student.unamur.be>
2019-10-19 00:51:51 +0200

    bob

--- src/db.go
+++ src/db.go
@@ -1,6 +1,43 @@
 package main
 
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// database pointer
+var db *gorm.DB
+
+// image table struct
 type Image struct {
+	ID    int32    `gorm:"primary_key:yes;column:ID"`
+	Path  string
+	Color string
+	Size  int
+}
+
+// init the database
+func initDB() {
+	var err error
+	db, err = gorm.Open("sqlite3", "salut.db")
+	// if there is an error, throw the exception
+	if err != nil {
+		panic(err)
+	}
+
+
+	// migrate the table
+	db.AutoMigrate(&Image{})
+
+	// get sample values
+	populateDb()
+
+}
+
+// generate sample values for debugging purpose
+func populateDb() {
+	cat := Image{ID: 34567, Path: "./public/cat.jpg", Color: "0000FF", Size: 3}
+	db.Save(&cat)
 }
 
 func getImages(color string, size int32) []string {
--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	initDB()
 	r := gin.Default()
 	r.GET("/add", routeAddImage)
 	r.Run() // listen and serve on 0.0.0.0:8080
