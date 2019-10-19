9880e278afa024d5cc33f5ebcba11cb6b2834f11
vinhig <vincent.higginson@student.unamur.Be>
2019-10-19 08:45:05 +0200

    Connect to db.

--- src/audio.go
+++ src/audio.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 
 	"github.com/youpy/go-wav"
@@ -26,6 +27,7 @@ func compareSounds(path1 string, path2 string) bool {
 	if err == io.EOF {
 		return false
 	}
+	log.Println(len(samples1))
 
 	// L'autre truc aussi
 	samples2, err := reader2.ReadSamples()
@@ -41,7 +43,6 @@ func compareSounds(path1 string, path2 string) bool {
 	}
 
 	for i := 0; i < myMin(len(samples1), len(samples2)); i++ {
-		fmt.Println("Comparing", samples1[i], "and", samples2[i])
 		if cleverCompare(samples1[i].Values[0], samples2[i].Values[0], 32) && cleverCompare(samples1[i].Values[1], samples2[i].Values[1], 32) {
 
 		} else {
