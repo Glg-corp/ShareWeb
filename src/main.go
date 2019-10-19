90d59a0f11a53b96134ceaaf736feace76156c5d
Guillaume Nizet <guillaume.nizet@student.unamur.be>
2019-10-19 09:22:22 +0200

    blablabla

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-    compareImage("./src/1.png", "./src/2.png")
+    compareImage("./src/A1.png", "./src/A.png")
 	r := gin.Default()
 	r.GET("/add", routeAddImage)
     r.Run() // listen and serve on 0.0.0.0:8080
