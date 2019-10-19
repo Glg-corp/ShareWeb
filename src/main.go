9d5f0b9597bf29e25c546f427c1006a4aff7963c
Guillaume Nizet <guillaume.nizet@student.unamur.be>
2019-10-19 14:48:36 +0200

    merge

--- src/main.go
+++ src/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"log"
+	//"log"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    compareImage("./src/A1.png", "./src/A.png")
+    compareImage("./src/1.png", "./src/2.png")
 	initDB()
 	defer db.Close()
 	
 	r := gin.Default()
 	r.Use(static.Serve("/", static.LocalFile("./public", true)))
 	r.POST("/add", routeAddImage)
-	r.Run() // listen and serve on 0.0.0.0:8}
+	r.Run() // listen and serve on 0.0.0.0:8
+}
